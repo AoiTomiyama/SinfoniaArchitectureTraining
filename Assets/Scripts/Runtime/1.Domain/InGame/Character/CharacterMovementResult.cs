f4c1913bbe0b92c507bb9db3ca0f8027b9f67901
AoiTomiyama <TadoO.o2209@gmail.com>
2026-04-10 23:44:10 +0900

    [add] 移動結果・移動処理を実装

---
+++ Assets/Scripts/Runtime/1.Domain/InGame/Character/CharacterMovementResult.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SSTraining.Runtime.Domain.InGame.Character
+{
+    /// <summary>
+    ///     キャラクターが移動する際の計算結果を表す値オブジェクト。
+    ///     移動が成功したかどうか、移動の目標位置、移動速度などを含む。
+    /// </summary>
+    public struct CharacterMovementResult
+    {
+        public CharacterMovementResult(bool isSuccessful, Vector3 targetPosition, float movementSpeed)
+        {
+            IsSuccessful = isSuccessful;
+            TargetPosition = targetPosition;
+            MovementSpeed = movementSpeed;
+        }
+
+        /// <summary> 移動が成功したかどうかを表すプロパティ。 </summary>
+        public bool IsSuccessful { get; }
+
+        /// <summary> 移動の目標位置を表すプロパティ。 </summary>
+        public Vector3 TargetPosition { get; }
+        
+        /// <summary> 移動速度を表すプロパティ。 </summary>
+        public float MovementSpeed { get; }
+    }
+}
---
+++ Assets/Scripts/Runtime/2.Application/InGame/Character/CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using SSTraining.Runtime.Domain.InGame.Character;
+
+namespace SSTraining.Runtime.Application.InGame.Character
+{
+    /// <summary>
+    ///     キャラクターの移動に関する処理を担当するクラス。移動速度や移動方法など、キャラクターの移動に関連するロジックを実装する。
+    /// </summary>
+    public class CharacterMovement
+    {
+        public CharacterMovement(MovementParams movementParams)
+        {
+            _movementParams = movementParams;
+        }
+
+        /// <summary>
+        ///     キャラクターを移動させるためのメソッド。ターゲットの位置とキャラクターの現在の位置を受け取り、移動の結果を返す。
+        /// </summary>
+        /// <param name="targetPosition"></param>
+        /// <param name="characterPosition"></param>
+        /// <returns></returns>
+        public CharacterMovementResult Move(Vector3 targetPosition, Vector3 characterPosition)
+        {
+            float distance = Vector3.Distance(targetPosition, characterPosition);
+
+            // 攻撃範囲外であれば、対称の座標に向けて移動する。
+            if (distance > _movementParams.AttackRange.Value)
+            {
+                return new CharacterMovementResult(
+                    true,
+                    characterPosition,
+                    _movementParams.MoveSpeed.Value
+                );
+            }
+
+            // 攻撃範囲内であれば、移動は行わない。
+            return new CharacterMovementResult(
+                false,
+                targetPosition,
+                0f
+            );
+        }
+
+        private readonly MovementParams _movementParams;
+    }
+}
