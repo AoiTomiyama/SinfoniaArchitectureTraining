7a577e9ab99691d8e9e16e14c09ce93684dcad42
AoiTomiyama <TadoO.o2209@gmail.com>
2026-04-11 16:39:40 +0900

    [update] レイアウト変更

--- Assets/Scripts/Runtime/5.InfraStructure/InGame/Character/CharacterDataAsset.cs
+++ Assets/Scripts/Runtime/5.InfraStructure/InGame/Character/CharacterDataAsset.cs
@@ -8,16 +8,33 @@ namespace SSTraining.Runtime.InfraStructure.InGame.Character
     [CreateAssetMenu(fileName = nameof(CharacterDataAsset), menuName = "SSTraining/Scriptable Object/CharacterData")]
     public class CharacterDataAsset : ScriptableObject
     {
-        [SerializeField, Tooltip("キャラクターの名前。")] private string _characterName;
-        [SerializeField, Tooltip("キャラクターの最大体力。")] private float _maxHealth;
-        [SerializeField, Tooltip("キャラクターの防御力。")] private float _defense;
-        [SerializeField, Tooltip("キャラクターの移動速度。")] private float _moveSpeed;
-        [SerializeField, Tooltip("キャラクターの攻撃力。")] private float _attackPower;
-        [SerializeField, Tooltip("キャラクターの攻撃範囲。")] private float _attackRange;
-        [SerializeField, Tooltip("キャラクターの攻撃速度。1秒あたりの攻撃回数を表す。")] private float _attackSpeed;
-        [SerializeField, Tooltip("キャラクターのクリティカルヒットが発生する確率。範囲は0から100。")] private float _criticalChance;
-        [SerializeField, Tooltip("キャラクターのクリティカルダメージ倍率。")] private float _criticalDamage;
-        
+        [SerializeField, Tooltip("キャラクターの名前。")]
+        private string _characterName;
+
+        [SerializeField, Tooltip("キャラクターの最大体力。")]
+        private float _maxHealth;
+
+        [SerializeField, Tooltip("キャラクターの防御力。")]
+        private float _defense;
+
+        [SerializeField, Tooltip("キャラクターの移動速度。")]
+        private float _moveSpeed;
+
+        [SerializeField, Tooltip("キャラクターの攻撃力。")]
+        private float _attackPower;
+
+        [SerializeField, Tooltip("キャラクターの攻撃範囲。")]
+        private float _attackRange;
+
+        [SerializeField, Tooltip("キャラクターの攻撃速度。1秒あたりの攻撃回数を表す。")]
+        private float _attackSpeed;
+
+        [SerializeField, Tooltip("キャラクターのクリティカルヒットが発生する確率。範囲は0から100。")]
+        private float _criticalChance;
+
+        [SerializeField, Tooltip("キャラクターのクリティカルダメージ倍率。")]
+        private float _criticalDamage;
+
         public string CharacterName => _characterName;
         public float MaxHealth => _maxHealth;
         public float Defense => _defense;
